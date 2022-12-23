a0337e7cc6fb9dd28efff421bcb350f535f23ab5
atrox3d <atrox3d@gmail.com>
2022-12-23 14:16:56 +0100

    analyze book chapters

--- nlp-sentiment.py
+++ nlp-sentiment.py
@@ -17,14 +17,30 @@ except LookupError as lue:
     analyzer = SentimentIntensityAnalyzer()
 
 # TODO https://stackoverflow.com/questions/62284611/sentiment-analysis-of-italian-sentences
-score = analyzer.polarity_scores("I hate trees")
-print(score)
-score = analyzer.polarity_scores("I like trees")
-print(score)
+text = "I hate trees"
+score = analyzer.polarity_scores(text)
+print(text, score)
+text = "I like trees"
+score = analyzer.polarity_scores(text)
+print(text, score)
 
 # load book
 with open('miracle_in_the_andes.txt', 'r', encoding='utf-8') as file:
     book = file.read()
 
 score = analyzer.polarity_scores(book)
-print(score)
+print("book", score)
+
+# extract chapters
+chapters = re.split("Chapter [0-9]+", book)[1:]
+# extract titles
+titles = re.findall('(Chapter [0-9]+)[\n]+(.+)', book)
+# format chapters
+titles = [f"{chapter}: {title}" for chapter, title in titles]
+print(chapters)
+print(titles)
+
+# print chapters scores
+for nr, (title, chapter) in enumerate(zip(titles, chapters), start=1):
+    score = analyzer.polarity_scores(chapter)
+    print(f'{nr:2d} - {title}: {score}')
